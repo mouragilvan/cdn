a65a9480f0936ec62fc10be6fcfe737d50ff51c8
Gilvan Moura <ti.gilvan@gmail.com>
2022-09-16 14:10:17 -0300

    Update blogger.js

--- blogger.js
+++ blogger.js
@@ -3,3 +3,9 @@
 var assuntos = document.getElementsByClassName("extendable");
 assuntos[0].setAttribute("open", "open");
 assuntos[0].style.cssText = "height: auto;";
+
+var marcador_expandir = document.getElementsByClassName("remaining-items");
+marcador_expandir[0].classList.add("expanded");
+
+var show_more = document.getElementsByClassName("show-more");
+show_more[0].classList.add("hidden");
